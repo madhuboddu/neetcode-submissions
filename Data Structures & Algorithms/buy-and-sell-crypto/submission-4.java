91558bb53d4db5cb7022ab11ae98a1307151896a
boddumadhu <b.madhu199@gmail.com>
2026-07-11 18:51:40 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+
+        int currprofit = 0;
+        int maxProfit = 0;
+        int buyInd = 0;
+
+        int minbuyPrice = 99999;
+        int curBp = 0;
+        for(int i = 0; i < prices.length;i++){
+
+            if(prices[i] < minbuyPrice ){
+
+                minbuyPrice = prices[i];
+                buyInd = i;
+            }else if(prices[i] > minbuyPrice ){
+
+                currprofit = prices[i] - prices[buyInd];
+
+                if(currprofit > maxProfit){
+                    maxProfit = currprofit;
+                }
+            }
+
+
+
+
+
+
+        }
+        return maxProfit;
+    }
+}
