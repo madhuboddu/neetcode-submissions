f62108749d438ebd741a6b9d0a451283590b4340
boddumadhu <b.madhu199@gmail.com>
2026-07-21 00:26:18 +0530

    Add: sort-colors - submission-4

---
+++ Data Structures & Algorithms/sort-colors/submission-4.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public void SortColors(int[] nums) {
+
+        int left = -1;
+        int right = nums.Length;
+        int temp = 0;
+        int i = 0;
+
+        while(i < right ){
+
+            if(nums[i] == 0){
+                left++;
+                temp = nums[i];
+                nums[i] = nums[left];
+                nums[left] = temp;
+                i++;
+                
+            }else if (nums[i] == 2){
+                right--;
+                temp = nums[i];
+                nums[i] = nums[right];
+                nums[right] = temp;
+                
+            }else{
+                i++;
+            }
+
+            
+        }
+   
+    }
+
+    
+}
